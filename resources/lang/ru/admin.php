885628ae6fab9acfb66c02323ab1355e48e79d8c
AlexRahvalov <125669715+AlexRahvalov@users.noreply.github.com>
2024-01-17 09:46:58 +0300

    Added Russian localization (#8)
    Added Russian translation

---
+++ resources/lang/ru/admin.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+	'nav' => [
+		'title' => 'AdvancedBan',
+
+		'settings' => 'Настройки',
+	],
+
+	'permissions' => [
+		'admin' => 'Управление плагином AdvancedBan',
+		'view' => 'Посмотреть страницу AdvancedBan',
+	],
+
+	'settings' => [
+		'title' => 'AdvancedBan страница настроек',
+
+		'host' => 'Хост',
+		'port' => 'Порт',
+		'database' => 'База данных',
+		'username' => 'Имя пользователя',
+		'password' => 'пароль',
+		'perPage' => 'Записей на странице',
+		'historyTable' => 'таблица истории',
+		'punishmentTable' => 'Таблица наказаний',
+		'route' => 'Route',
+	],
+];
